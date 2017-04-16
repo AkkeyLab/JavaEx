ec1191b45e48d6a90ef7142f22ebeb03d87ac07e
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-16 20:13:12 +0900

    Add print string lesson.

--- JavaLesson/src/lesson/chap00/ALesson.java
+++ JavaLesson/src/lesson/chap00/ALesson.java
@@ -1,5 +1,12 @@
 package lesson.chap00;
 
 public class ALesson {
-
+	public static void main(String[] args) {
+		println("Hello world");
+	}
+	
+	// 文字出力が簡単にできるメソッド
+	public static void println(String s) {
+		System.out.println(s);
+	}
 }
