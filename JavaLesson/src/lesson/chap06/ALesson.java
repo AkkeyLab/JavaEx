a5b9f1b75a2a4db3d68eb878cde91ae171e3fafe
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-17 00:20:38 +0900

    Add new class. Use endsWith function.

---
+++ JavaLesson/src/lesson/chap06/ALesson.java
@@ -0,0 +1,27 @@
+package lesson.chap06;
+
+import java.io.*;
+
+public class ALesson {
+	public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String inputString;
+            // 未入力になるまで繰り返す
+            while ((inputString = reader.readLine()) != null) {
+            	// endsWithでは文字列の終わりが引数と等しいかを判定する
+            	// ちなみに、文字列のはじめに関してはstartsWithを使用して判定する
+                if (inputString.endsWith("{") || inputString.endsWith("}")) {
+                    println(inputString);
+                }
+            }
+        } catch (IOException e) {
+            println("" + e);
+        }
+	}
+	
+	// 文字出力が簡単にできるメソッド
+	public static void println(String s) {
+		System.out.println(s);
+	}
+}
