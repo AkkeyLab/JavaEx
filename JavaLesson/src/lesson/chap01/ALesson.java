434204f189fe1e326bf7bdd4f402126674feac83
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-16 20:21:13 +0900

    Add new class.

---
+++ JavaLesson/src/lesson/chap01/ALesson.java
@@ -0,0 +1,14 @@
+package lesson.chap01;
+
+public class ALesson {
+	public static void main(String[] args) {
+		// 整数直後に「L」を付けることでlong intとして扱うことができる
+		// Swiftならasで型を指定してあげればいいと思う
+		println("" + (100000L * 100000L));
+	}
+	
+	// 文字出力が簡単にできるメソッド
+	public static void println(String s) {
+		System.out.println(s);
+	}
+}
