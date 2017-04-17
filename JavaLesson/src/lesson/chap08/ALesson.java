3b2d0a5197df6f74bfa8644acea00090df84ad7d
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-17 14:59:40 +0900

    Add comment

--- JavaLesson/src/lesson/chap08/ALesson.java
+++ JavaLesson/src/lesson/chap08/ALesson.java
@@ -4,6 +4,11 @@ public class ALesson {
 	public static void main(String[] args) {
 		double[] data = { 3.1, 4.1, 5.9, 2.6, 5.3, 9.7 };
 		double sum = 0.0;
+
+		// データ型 変数名: コレクション
+
+		// Swiftでは以下のように書く。ほぼ同じ。
+		// for d in data {
 		for (double d : data) {
 			sum += d;
 		}
