72488f738d6b62f40dbaf849e4548d2762680500
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-04-20 01:02:06 +0900

    Refactoring

--- JavaLesson/src/lesson/chap10/GamePlayer.java
+++ JavaLesson/src/lesson/chap10/GamePlayer.java
@@ -4,14 +4,6 @@ public class GamePlayer {
 	public String playername;
 	// このように、staticで宣言した場合、新しくオブジェクト（インスタンス）を作成しても、すべてのオブジェクトで値が共有される
 	public static String commonName;
-	
-	/* 実行結果
-	   [player:Mad Hatter] [common:Itaya]
-	   [player:March Hare] [common:Itaya]
-	   [player:Alice] [common:Itaya]
-	   
-	   インスタンスが異なるにも関わらず、staticで宣言していた変数の値が共有されているのがわかる
-	 */
 
 	public GamePlayer(String name, String common) {
 		playername = name;
@@ -21,20 +13,5 @@ public class GamePlayer {
 	@Override
 	public String toString() {
 		return "[player:" + playername + "] [common:" + commonName + "]";
-	}
-
-	public static void main(String[] args) {
-		GamePlayer[] player = new GamePlayer[3];
-		player[0] = new GamePlayer("Mad Hatter", "Akkey");
-		player[1] = new GamePlayer("March Hare", "Akio");
-		player[2] = new GamePlayer("Alice", "Itaya");
-		for (int i = 0; i < player.length; i++) {
-			println("" + player[i]);
-		}
-	}
-
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
 	}
 }
