bd4bf5275d9e638d70c6dbbac4aeb0dc405b0793
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-07 01:24:32 +0900

    Refactoring and string extraction for chap12. And update eclipse setting file.

--- JavaLesson/src/lesson/chap12/ALesson.java
+++ JavaLesson/src/lesson/chap12/ALesson.java
@@ -1,29 +1,43 @@
 package lesson.chap12;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 7, 2017
+ */
 public class ALesson {
-	public static void main(String[] args) {
-		int[] myarray = new int[3];
-		// 確保していないアドレスへのアクセスを試みる
-		// もちろんエラーが返ってくる
-		myAssign(myarray, 100, 0);
-		println("END");
-	}
 
-	static void myAssign(int[] arr, int index, int value) {
-		println("myAssign");
-		try {
-			println("Let's input value");
-			arr[index] = value;
-			println("Value input Completed");
-		} catch (ArrayIndexOutOfBoundsException e) {
-			println("Exception");
-			println("Exception : " + e);
-		}
-		println("return");
-	}
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    int[] myarray = new int[3];
+    // 確保していないアドレスへのアクセスを試みる
+    // もちろんエラーが返ってくる
+    myAssign(myarray, 100, 0);
+    println(Messages.getString("ALesson.0")); //$NON-NLS-1$
+  }
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  static void myAssign(int[] arr, int index, int value) {
+    println(Messages.getString("ALesson.1")); //$NON-NLS-1$
+    try {
+      println(Messages.getString("ALesson.2")); //$NON-NLS-1$
+      arr[index] = value;
+      println(Messages.getString("ALesson.3")); //$NON-NLS-1$
+    } catch (ArrayIndexOutOfBoundsException e) {
+      println(Messages.getString("ALesson.4")); //$NON-NLS-1$
+      println(Messages.getString("ALesson.5") + e); //$NON-NLS-1$
+    }
+    println(Messages.getString("ALesson.6")); //$NON-NLS-1$
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
--- JavaLesson/src/lesson/chap12/CLesson.java
+++ JavaLesson/src/lesson/chap12/CLesson.java
@@ -1,20 +1,39 @@
 package lesson.chap12;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 7, 2017
+ */
 public class CLesson {
-	public static void main(String[] args) {
-		println("" + factorial(10));
-	}
 
-	public static int factorial(int n) {
-		if (n <= 0) {
-			return 1;
-		} else {
-			return n * factorial(n - 1);
-		}
-	}
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    println("" + factorial(10)); //$NON-NLS-1$
+  }
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * 階乗計算を行うメソッド
+   * 
+   * @param n 数値
+   * @return 階乗結果
+   */
+  public static int factorial(int n) {
+    if (n <= 0) {
+      return 1;
+    }
+    return n * factorial(n - 1);
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
