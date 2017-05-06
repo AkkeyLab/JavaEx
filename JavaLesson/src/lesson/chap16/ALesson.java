a9b81f1a057338982624836ca4257abee49c84c7
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-07 01:53:16 +0900

    Refactoring and string extraction for chap16. And update eclipse setting file.

--- JavaLesson/src/lesson/chap16/ALesson.java
+++ JavaLesson/src/lesson/chap16/ALesson.java
@@ -2,36 +2,54 @@ package lesson.chap16;
 
 import java.io.*;
 
-public class ALesson {
-	public static void main(String[] args) {
-		if (args.length != 2) {
-			println("How to use：java ALesson SearchString SearchFiles");
-			println("ex：java ALesson System ALesson.java");
-			System.exit(0);
-		}
-		String findstring = args[0];
-		String filename = args[1];
-		try {
-			String line;
-			int linenum = 1;
-			BufferedReader reader = new BufferedReader(new FileReader(filename));
-			while ((line = reader.readLine()) != null) {
-				int n = line.indexOf(findstring);
-				if (n >= 0) {
-					System.out.println(linenum + ":" + line);
-				}
-				linenum++;
-			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			println(filename + ": Not found!");
-		} catch (IOException e) {
-			println("" + e);
-		}
-	}
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+/**
+ * @author akkey
+ * @version $Revision$, May 7, 2017
+ */
+public class ALesson {
+
+  private static BufferedReader reader;
+
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      println(Messages.getString("ALesson.0")); //$NON-NLS-1$
+      println(Messages.getString("ALesson.1")); //$NON-NLS-1$
+      System.exit(0);
+    }
+    String findstring = args[0];
+    String filename = args[1];
+    try {
+      String line;
+      int linenum = 1;
+      reader = new BufferedReader(new FileReader(filename));
+      while ((line = reader.readLine()) != null) {
+        int n = line.indexOf(findstring);
+        if (n >= 0) {
+          System.out.println(linenum + Messages.getString("ALesson.2") + line); //$NON-NLS-1$
+        }
+        linenum++;
+      }
+      reader.close();
+    } catch (FileNotFoundException e) {
+      println(filename + Messages.getString("ALesson.3")); //$NON-NLS-1$
+      println("" + e); //$NON-NLS-1$
+    } catch (IOException e) {
+      println("" + e); //$NON-NLS-1$
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
