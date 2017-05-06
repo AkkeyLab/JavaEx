23e857530647a99b6af4b1e87e25eb3244f27dd9
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-06 23:33:10 +0900

    Refactoring and string extraction for chap03. And update eclipse setting file.

--- JavaLesson/src/lesson/chap03/ALesson.java
+++ JavaLesson/src/lesson/chap03/ALesson.java
@@ -2,40 +2,58 @@ package lesson.chap03;
 
 import java.io.*;
 
+
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class ALesson {
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		println("現在時刻24時間法で時間のみ入力してください");
-		try {
-			// tryが必要なのはこの2つ
-			String inputString = reader.readLine();
-			int n = Integer.parseInt(inputString);
+  /**
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-			println(greeting(n));
-		} catch (IOException e) {
-			println("" + e);
-		} catch (NumberFormatException e) {
-			println("数字の形式が正しくありません");
-		}
-	}
+    println(Messages.getString("ALesson.0")); //$NON-NLS-1$
+    try {
+      // tryが必要なのはこの2つ
+      String inputString = reader.readLine();
+      int n = Integer.parseInt(inputString);
 
-	public static String greeting(int n) {
-		if (0 <= n && n <= 11) {
-			return "おはようございます";
-		} else if (n == 12) {
-			return "お昼です";
-		} else if (13 <= n && n <= 18) {
-			return "こんにちは";
-		} else if (19 <= n && n <= 23) {
-			return "こんばんは";
-		} else {
-			return "時刻の範囲を越えています";
-		}
-	}
+      println(greeting(n));
+    } catch (IOException e) {
+      println("" + e); //$NON-NLS-1$
+    } catch (NumberFormatException e) {
+      println(Messages.getString("ALesson.2")); //$NON-NLS-1$
+      println("" + e); //$NON-NLS-1$
+    }
+  }
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * @param n 時間
+   * @return 時間に対応した挨拶文字列
+   */
+  public static String greeting(int n) {
+    if (0 <= n && n <= 11) {
+      return Messages.getString("ALesson.3"); //$NON-NLS-1$
+    } else if (n == 12) {
+      return Messages.getString("ALesson.4"); //$NON-NLS-1$
+    } else if (13 <= n && n <= 18) {
+      return Messages.getString("ALesson.5"); //$NON-NLS-1$
+    } else if (19 <= n && n <= 23) {
+      return Messages.getString("ALesson.6"); //$NON-NLS-1$
+    } else {
+      return Messages.getString("ALesson.7"); //$NON-NLS-1$
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
