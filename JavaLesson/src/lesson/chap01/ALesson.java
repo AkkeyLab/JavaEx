c3c1370ffa4880ab4abf01c55db59fbe30bbe3ee
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-06 21:21:26 +0900

    Refactoring and string extraction for chap01.

--- JavaLesson/src/lesson/chap01/ALesson.java
+++ JavaLesson/src/lesson/chap01/ALesson.java
@@ -1,14 +1,26 @@
 package lesson.chap01;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class ALesson {
-	public static void main(String[] args) {
-		// 整数直後に「L」を付けることでlong intとして扱うことができる
-		// Swiftならasで型を指定してあげればいいと思う
-		println("" + (100000L * 100000L));
-	}
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    // 整数直後に「L」を付けることでlong intとして扱うことができる
+    // Swiftならasで型を指定してあげればいいと思う
+    println("" + (100000L * 100000L)); //$NON-NLS-1$
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
--- JavaLesson/src/lesson/chap01/BLesson.java
+++ JavaLesson/src/lesson/chap01/BLesson.java
@@ -1,29 +1,44 @@
 package lesson.chap01;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class BLesson {
-	public static void main(String[] args) {
-		product();
-	}
 
-	// 0x0から10x10までを計算して出力するメソッド
-	public static void product() {
-		// Swiftであるとこのフォーマットでfor文を書くことができなくなった
-		for (int i = 0; i <= 10; i++) {
-			println("" + i + "x" + i + "=" + (i * i));
-		}
-	}
+  /**
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    product();
+  }
 
-	// C言語風にフォーマット指定で記述
-	// Swiftでもこのようなフォーマット方法が使用できる。
-	public static void product_c() {
-		for (int i = 0; i <= 10; i++) {
-			// クラス名で区別してるんだ
-			System.out.printf("%d × %d = %d%n", i, i, i * i);
-		}
-	}
+  /**
+   * 0x0から10x10までを計算して出力するメソッド
+   */
+  public static void product() {
+    // Swiftであるとこのフォーマットでfor文を書くことができなくなった
+    for (int i = 0; i <= 10; i++) {
+      println("" + i + Messages.getString("BLesson.1") + i + Messages.getString("BLesson.2") + (i * i)); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+    }
+  }
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * C言語風にフォーマット指定で記述 Swiftでもこのようなフォーマット方法が使用できる。
+   */
+  public static void product_c() {
+    for (int i = 0; i <= 10; i++) {
+      // クラス名で区別してるんだ
+      System.out.printf("%d × %d = %d%n", i, i, i * i); //$NON-NLS-1$
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
