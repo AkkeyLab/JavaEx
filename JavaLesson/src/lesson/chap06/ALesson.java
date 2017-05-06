13ba8befc2a2af13fb97df43a05e81afddefdfa8
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-07 00:12:49 +0900

    Refactoring and update eclipse setting file for chap06.

--- JavaLesson/src/lesson/chap06/ALesson.java
+++ JavaLesson/src/lesson/chap06/ALesson.java
@@ -2,26 +2,41 @@ package lesson.chap06;
 
 import java.io.*;
 
-public class ALesson {
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		try {
-			String inputString;
-			// 未入力になるまで繰り返す
-			while ((inputString = reader.readLine()) != null) {
-				// endsWithでは文字列の終わりが引数と等しいかを判定する
-				// ちなみに、文字列のはじめに関してはstartsWithを使用して判定する
-				if (inputString.endsWith("{") || inputString.endsWith("}")) {
-					println(inputString);
-				}
-			}
-		} catch (IOException e) {
-			println("" + e);
-		}
-	}
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+/**
+ * @author akkey
+ * @version $Revision$, May 7, 2017
+ */
+public class ALesson {
+
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    try {
+      String inputString;
+      // 未入力になるまで繰り返す
+      while ((inputString = reader.readLine()) != null) {
+        // endsWithでは文字列の終わりが引数と等しいかを判定する
+        // ちなみに、文字列のはじめに関してはstartsWithを使用して判定する
+        if (inputString.endsWith("{") || inputString.endsWith("}")) { //$NON-NLS-1$ //$NON-NLS-2$
+          println(inputString);
+        }
+      }
+    } catch (IOException e) {
+      println("" + e); //$NON-NLS-1$
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
--- JavaLesson/src/lesson/chap06/BLesson.java
+++ JavaLesson/src/lesson/chap06/BLesson.java
@@ -1,28 +1,46 @@
 package lesson.chap06;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 7, 2017
+ */
 public class BLesson {
-	public static void main(String[] args) {
-		int i = 0;
 
-		// 10未満である限り繰り返す
-		while (i < 10) {
-			int j = 0;
-			while (j < i * i) {
-				print("*");
-				j++;
-			}
-			i++;
-			println("");
-		}
-	}
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    int i = 0;
 
-	// 改行なし
-	public static void print(String s) {
-		System.out.print(s);
-	}
+    // 10未満である限り繰り返す
+    while (i < 10) {
+      int j = 0;
+      while (j < i * i) {
+        print("*"); //$NON-NLS-1$
+        j++;
+      }
+      i++;
+      println(""); //$NON-NLS-1$
+    }
+  }
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * 改行を行わない文字出力
+   * 
+   * @param s 出力する文字列
+   */
+  public static void print(String s) {
+    System.out.print(s);
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
