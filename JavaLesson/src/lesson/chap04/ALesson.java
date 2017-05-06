017bce8e48a784f378e8c23949f197d120b2d644
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-06 23:47:29 +0900

    Refactoring and string extraction for chap04. And update eclipse setting file.

--- JavaLesson/src/lesson/chap04/ALesson.java
+++ JavaLesson/src/lesson/chap04/ALesson.java
@@ -2,54 +2,79 @@ package lesson.chap04;
 
 import java.io.*;
 
+
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class ALesson {
-	public static void main(String[] args) {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		outQuestion();
-		try {
-			String inputString = reader.readLine();
-			int n = Integer.parseInt(inputString);
-			outAns(n);
+  /**
+   * 実行開始地点
+   * 
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		} catch (IOException e) {
-			println("" + e);
-		} catch (NumberFormatException e) {
-			println("数字の形式が正しくありません");
-		}
-	}
+    outQuestion();
+    try {
+      String inputString = reader.readLine();
+      int n = Integer.parseInt(inputString);
+      outAns(n);
 
-	public static void outQuestion() {
-		String[] qStrings = { "飲み物は何が好きですか？", "1 オレンジジュース", "2 コーヒー", "3 ミルク", "4 どれでもない", "1, 2, 3, 4のどれかを選んでください" };
+    } catch (IOException e) {
+      println("" + e); //$NON-NLS-1$
+    } catch (NumberFormatException e) {
+      println(Messages.getString("ALesson.1")); //$NON-NLS-1$
+      println("" + e); //$NON-NLS-1$
+    }
+  }
 
-		for (int i = 0; i < qStrings.length; i++) {
-			println(qStrings[i]);
-		}
-	}
+  /**
+   * 質問文の出力を行うメソッド
+   */
+  public static void outQuestion() {
+    String[] qStrings = {Messages.getString("ALesson.2"), Messages.getString("ALesson.3"), Messages.getString("ALesson.4"), Messages.getString("ALesson.5"), Messages.getString("ALesson.6"), //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$//$NON-NLS-4$//$NON-NLS-5$
+        Messages.getString("ALesson.7")}; //$NON-NLS-1$
 
-	public static void outAns(int n) {
-		// C言語と同じと考えておけば問題ない
-		// Swiftでは貫通しない
+    for (int i = 0; i < qStrings.length; i++) {
+      println(qStrings[i]);
+    }
+  }
 
-		// あまり使わないとは思うが、比較に文字も最新Javaでは使用できる
-		switch (n) {
-		case 1:
-			System.out.println("オレンジジュースです");
-			break;
-		case 2:
-			System.out.println("コーヒーです");
-			break;
-		case 3:
-			System.out.println("ミルクです");
-			break;
-		default:
-			System.out.println("どれでもありません");
-			break;
-		}
-	}
+  /**
+   * 選択数値に応じた返答を文字列で出力するメソッド
+   * 
+   * @param n 返答数値
+   */
+  public static void outAns(int n) {
+    // C言語と同じと考えておけば問題ない
+    // Swiftでは貫通しない
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+    // あまり使わないとは思うが、比較に文字も最新Javaでは使用できる
+    switch (n) {
+      case 1:
+        System.out.println(Messages.getString("ALesson.8")); //$NON-NLS-1$
+        break;
+      case 2:
+        System.out.println(Messages.getString("ALesson.9")); //$NON-NLS-1$
+        break;
+      case 3:
+        System.out.println(Messages.getString("ALesson.10")); //$NON-NLS-1$
+        break;
+      default:
+        System.out.println(Messages.getString("ALesson.11")); //$NON-NLS-1$
+        break;
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
---
+++ JavaLesson/src/lesson/chap04/Messages.java
@@ -0,0 +1,36 @@
+/*
+ * Created on May 6, 2017
+ * Copyright (C) 2017 Koga Laboratory. All rights reserved.
+ *
+ */
+package lesson.chap04;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
+public class Messages {
+
+  private static final String BUNDLE_NAME = "lesson.chap04.messages"; //$NON-NLS-1$
+
+  private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+  private Messages() {}
+
+  /**
+   * @param key 文字列識別子
+   * @return 文字列
+   */
+  public static String getString(String key) {
+    try {
+      return RESOURCE_BUNDLE.getString(key);
+    } catch (MissingResourceException e) {
+      System.out.println(e);
+      return '!' + key + '!';
+    }
+  }
+}
