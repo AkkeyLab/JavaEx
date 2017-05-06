de561eda54feeb282879ff122e84ce311da0301d
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-06 21:22:33 +0900

    Refactoring and string extraction for chap02.

--- JavaLesson/src/lesson/chap02/BLesson.java
+++ JavaLesson/src/lesson/chap02/BLesson.java
@@ -2,30 +2,43 @@ package lesson.chap02;
 
 import java.io.*;
 
+
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class BLesson {
-	public static void main(String[] args) {
-		// 文字入力支援
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-		try {
-			println("好きな文字列を入力してみてください");
-			String inputString = reader.readLine();
+  /**
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    // 文字入力支援
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-			// .lengthで文字列の長さを取得することができる
-			for (int i = 0; i < inputString.length(); i++) {
-				// charAtに何番目の文字を取得したいかを指定して、文字を取得s
-				char c = inputString.charAt(i);
-				// int型にキャストしてあげることで文字コードを取得することが可能
-				// C言語みたい
-				println("'" + c + "' の文字コードは " + (int) c + "です");
-			}
-		} catch (IOException e) {
-			println("" + e);
-		}
-	}
+    try {
+      println(Messages.getString("BLesson.0")); //$NON-NLS-1$
+      String inputString = reader.readLine();
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+      // .lengthで文字列の長さを取得することができる
+      for (int i = 0; i < inputString.length(); i++) {
+        // charAtに何番目の文字を取得したいかを指定して、文字を取得s
+        char c = inputString.charAt(i);
+        // int型にキャストしてあげることで文字コードを取得することが可能
+        // C言語みたい
+        println(Messages.getString("BLesson.1") + c + Messages.getString("BLesson.2") + (int)c + Messages.getString("BLesson.3")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+      }
+    } catch (IOException e) {
+      println("" + e); //$NON-NLS-1$
+    }
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
--- JavaLesson/src/lesson/chap02/CLesson.java
+++ JavaLesson/src/lesson/chap02/CLesson.java
@@ -1,13 +1,25 @@
 package lesson.chap02;
 
+/**
+ * @author akkey
+ * @version $Revision$, May 6, 2017
+ */
 public class CLesson {
-	public static void main(String[] args) {
-		// 基本的に制度が高い数値に合わせて結果が出力される
-		println("光が1日に進む距離は" + (300000L * 60 * 60 * 24) + "kmです");
-	}
 
-	// 文字出力が簡単にできるメソッド
-	public static void println(String s) {
-		System.out.println(s);
-	}
+  /**
+   * @param args コマンドライン引数
+   */
+  public static void main(String[] args) {
+    // 基本的に制度が高い数値に合わせて結果が出力される
+    println(Messages.getString("CLesson.0") + (300000L * 60 * 60 * 24) + Messages.getString("CLesson.1")); //$NON-NLS-1$ //$NON-NLS-2$
+  }
+
+  /**
+   * 文字出力が簡単にできるメソッド
+   * 
+   * @param s 出力する文字列
+   */
+  public static void println(String s) {
+    System.out.println(s);
+  }
 }
