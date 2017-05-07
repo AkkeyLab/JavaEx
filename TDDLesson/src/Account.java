ee03b126442bd08a84c77281309dc1b6518e057b
AKIO <akio.itaya7@kne.biglobe.ne.jp>
2017-05-08 00:30:46 +0900

    Add red plate test case. Step 1.

---
+++ TDDLesson/src/Account.java
@@ -0,0 +1,24 @@
+
+/*
+ * Created on May 8, 2017
+ * Copyright (C) 2017 Koga Laboratory. All rights reserved.
+ *
+ */
+
+/**
+ * @author akkey
+ * @version $Revision$, May 8, 2017
+ */
+public class Account {
+
+  /**
+   * 合計金額を取得するメソッド
+   * 
+   * @return 合計金額
+   */
+  public int getTotalPrice() {
+    // TODO 自動生成されたメソッド・スタブ
+    return 100;
+  }
+
+}
---
+++ TDDLesson/src/AccountTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+
+/*
+ * Created on May 8, 2017
+ * Copyright (C) 2017 Koga Laboratory. All rights reserved.
+ *
+ */
+
+/**
+ * @author akkey
+ * @version $Revision$, May 8, 2017
+ */
+public class AccountTest extends TestCase {
+
+  /**
+   * 赤皿に関するメソッド
+   */
+  @Test
+  public void testRedPlate() {
+    Account account = new Account();
+
+    assertEquals(100, account.getTotalPrice());
+  }
+
+}
